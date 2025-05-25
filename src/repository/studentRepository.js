cc34316499969c764ab9efa1ed1a97c4a68c8c6f
luv2s <ox_m@list.ru>
2025-05-25 15:05:13 +0300

    first mongo db server connect

--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -1,29 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import {MongoClient} from "mongodb";
-// const url = 'mongodb://edd:1234@localhost:27017/java59?authSource=admin';
-//
-// dotenv.config();
-
-const dbName = 'java59';
-const client = new MongoClient(process.env.MONGO_URI);
 let collection;
 
-export async function connect() { // подключаем базу данных
-    // if (!(client.topology && client.topology.isConnected())) {
-    //     await client.connect();
-    // }
-    if (!client.topology?.isConnected()) { // проверка на то, создана ли база (топология) и приконектилась ли база
-        await client.connect(); // получить коллекцию
-    }
-    const db = client.db(dbName);  // сщздали коллекцию
-    collection = db.collection("college"); // дали название коллекции, инициализируем
+export function init(db){
+    collection = db.collection("college");
 }
 
-
 export const addStudent = async ({id, name, password}) => {
-    await connect();
     const existing = await collection.findOne({_id: id});
     if (existing) {
         return false;
@@ -36,18 +17,15 @@ export const addStudent = async ({id, name, password}) => {
 }
 
 export const findStudent = async (id) => {
-    await connect();
     return await collection.findOne({_id: id});
 }
 
-export const deleteStudent = async (id) => {
-    await connect();
+export const deleteStudent = async (id) => {    ;
     return await collection.findOneAndDelete({_id: id});
     //найти и удалить
 }
 
 export const updateStudent = async (id, data) => {
-    await connect();
     return await collection.findOneAndUpdate( // найти и обновить
         {_id: id},
         {$set: data},
@@ -64,7 +42,6 @@ export const addScore = async (id, exam, score) => {
 }
 
 export const findByName = async (name) => {
-    await connect();
     return await collection.find({name: {$regex: `^${name}$`, $options: 'i'}}).toArray();
     //найди нэйм, но игнорируй регистр
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
 import express from 'express';
 import studentRoutes from "./routes/studentRoutes.js";
+import {MongoClient} from "mongodb";
+import {init} from "./repository/studentRepository.js";
 
+dotenv.config();
 const app = express();
 const port = 8080;
+const dbName = 'java59';
+const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(studentRoutes);
@@ -10,6 +16,16 @@ app.use((req, res) => {
     res.status(404).type('text/plain; charset=utf-8').send('Not Found');
 })
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}. Press Ctrl-C to finish`);
-})
+async function startServer() {
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        init(db);
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}. Press Ctrl-C to finish`);
+        })
+    }catch(err){
+        console.log('Failed to connect to MongoDB', err);
+    }
+}
+startServer();
